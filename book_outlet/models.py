6f308a4d98fe7f053e001900b720fadb856cd9b4
BillStephens2022 <two4onebill@yahoo.com>
2023-12-24 10:05:25 -0500

    set up admin page for updating the Books database

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -11,14 +11,15 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=50)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
     
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    # code below no longer needed after admin.py is set up
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super().save(*args, **kwargs)
     
 
     def __str__(self):
